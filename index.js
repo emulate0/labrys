758e668edff58cf17a52f52766a3e37fc1cacbfd
emulate0 <coffeeboy121@gmail.com>
2020-11-14 13:33:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const manager = new discord.ShardingManager('./bot.js', { token: process.env.TOKEN });
+const manager = new discord.ShardingManager('./bot.js', { token: process.env.bot_token });
 manager.on('shardCreate', (shard) => console.log);
 manager.spawn();
