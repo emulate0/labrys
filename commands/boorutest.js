c2fad91308ba5f27af8447f8edfca17c8368312d
typack <39818656+emulate0@users.noreply.github.com>
2020-11-15 23:06:27 -0600

    Update boorutest.js

--- commands/boorutest.js
+++ commands/boorutest.js
@@ -5,9 +5,7 @@ module.exports = {
 	names: ['boorutest'],
 	usage: 'boorutest [name]',
 	description: 'Testing out the Booru.',
-	execute: (message, args) => {
-		const booruUrl = '';
-		
+	execute: (message, args) => {	
 		return message.channel.send(new MessageEmbed()
 			.setColor(message.client.colors.NSFW)
 			.setTitle('Booru')
@@ -15,21 +13,5 @@ module.exports = {
 			.addField('API', `${Math.round(message.client.ws.ping)}ms`, true)
 		).catch((error) => console.log);
 		
-		Booru.search('safebooru', 'glaceon', { limit: 1, random: true })
-  .then(posts => {
-    if (posts.length === 0) {
-      console.log('No images found.')
-    }
-
-    for (let post of posts) {
-	  booruUrl = post.fileUrl.toString
-      console.log(post.fileUrl)
-    }
-  });
-		
-		return message.channel.send(new messageEmbed()
-			.setColor(message.client.colors.NSFW)
-			.setTitle('Booru')
-			.setImage(booruUrl)
 	}
 };
