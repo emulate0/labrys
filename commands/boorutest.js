9abe9b0c4495f1d3e7c03f8b7f046a1e43990e62
typack <39818656+emulate0@users.noreply.github.com>
2020-11-15 23:25:04 -0600

    Update boorutest.js

--- commands/boorutest.js
+++ commands/boorutest.js
@@ -7,11 +7,9 @@ module.exports = {
 	description: 'Testing out the Booru.',
 	execute: (message, args) => {	
 		return message.channel.send(new MessageEmbed()
-		
 			.setColor(message.client.colors.NSFW)
 			.setTitle('Booru')
 			.addImage('https://cdn.bulbagarden.net/upload/thumb/2/23/471Glaceon.png/250px-471Glaceon.png')
-			
 		).catch((error) => console.log);
 		
 	}
