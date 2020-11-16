15606420a141efdbe9e3bfc8e56edd1ec162189f
typack <39818656+emulate0@users.noreply.github.com>
2020-11-16 11:48:09 -0600

    Update boorutest.js

--- commands/boorutest.js
+++ commands/boorutest.js
@@ -5,10 +5,13 @@ module.exports = {
 	names: ['boorutest'],
 	usage: 'boorutest [name]',
 	description: 'Testing out the Booru.',
-	execute: (message, args) => {	
+	execute: (message, args) => {
+		async function booruSearch(site, tags, limit = 1, random = true) {
+			const posts = await Booru.search(site, tags, {limit, random})
+
+			return console.log(posts[0].fileUrl)
+		}	
 		var imgLink = 'https://cdn.shopify.com/s/files/1/1061/1924/products/Woman_Saying_Hello_Emoji_grande.png?v=1571606062'
-		const posts = await Booru.search('safebooru', ['cat'], {limit: 3, random: true})
-			console.log(posts[0].fileUrl)
 		return message.channel.send(new MessageEmbed()
 			.setColor(message.client.colors.NSFW)
 			.setTitle('Booru' + imgLink)
