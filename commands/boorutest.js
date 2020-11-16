92fad0b5211fe845715bf8377076ca9a4fd12514
typack <39818656+emulate0@users.noreply.github.com>
2020-11-15 22:56:47 -0600

    Update boorutest.js

--- commands/boorutest.js
+++ commands/boorutest.js
@@ -26,16 +26,7 @@ module.exports = {
       console.log(post.fileUrl)
     }
   })
-  .catch(err => {
-    if (err instanceof BooruError) {
-      // It's a custom error thrown by the package
-      // Typically results from errors the boorus returns, eg. "too many tags"
-      console.error(err)
-    } else {
-      // This means something pretty bad happened
-      console.error(err)
-    }
-  })
+  .catch(error) => console.log);
 		
 		return message.channel.send(new messageEmbed()
 			.setColor(message.client.colors.NSFW)
