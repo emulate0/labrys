4a9218614d89193bef400a9b3a7c10b4d36eec83
typack <39818656+emulate0@users.noreply.github.com>
2020-11-16 12:01:57 -0600

    Update boorutest.js

--- commands/boorutest.js
+++ commands/boorutest.js
@@ -1,17 +1,18 @@
 const { MessageEmbed } = require('discord.js');
 const Booru = require('booru')
 
+async function booruSearch(site, tags, limit = 1, random = true) {
+	const posts = await Booru.search(site, tags, {limit, random})
+
+	return posts[0].fileUrl
+}	
+
 module.exports = {
 	names: ['boorutest'],
 	usage: 'boorutest [name]',
 	description: 'Testing out the Booru.',
 	execute: (message, args) => {
 		var imgLink = 'https://cdn.shopify.com/s/files/1/1061/1924/products/Woman_Saying_Hello_Emoji_grande.png?v=1571606062'
-		async function booruSearch(site, tags, limit = 1, random = true) {
-			const posts = await Booru.search(site, tags, {limit, random})
-
-			imgLink = posts[0].fileUrl
-		}	
 		booruSearch('safebooru', ['cat'])
 		return message.channel.send(new MessageEmbed()
 			.setColor(message.client.colors.NSFW)
