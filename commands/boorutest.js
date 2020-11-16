69cf26903ff80929dd82770c593b44ddecf79c1c
typack <39818656+emulate0@users.noreply.github.com>
2020-11-15 23:27:05 -0600

    Update boorutest.js

--- commands/boorutest.js
+++ commands/boorutest.js
@@ -5,12 +5,11 @@ module.exports = {
 	names: ['boorutest'],
 	usage: 'boorutest [name]',
 	description: 'Testing out the Booru.',
-	execute: (message, args) => {	
+	execute: (message, args) => {
 		return message.channel.send(new MessageEmbed()
 			.setColor(message.client.colors.NSFW)
 			.setTitle('Booru')
-			.addImage('https://cdn.bulbagarden.net/upload/thumb/2/23/471Glaceon.png/250px-471Glaceon.png')
+			.setImage('https://cdn.bulbagarden.net/upload/thumb/2/23/471Glaceon.png/250px-471Glaceon.png')
 		).catch((error) => console.log);
-		
 	}
 };
