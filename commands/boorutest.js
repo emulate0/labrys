6c9b2b19b0087259b15d000631cf2fa172e6b9eb
typack <39818656+emulate0@users.noreply.github.com>
2020-11-15 22:42:58 -0600

    Update boorutest.js

--- commands/boorutest.js
+++ commands/boorutest.js
@@ -11,27 +11,6 @@ module.exports = {
 			.setTitle('Booru')
 			.addField('Client', `${new Date() - message.createdAt}ms`, true)
 			.addField('API', `${Math.round(message.client.ws.ping)}ms`, true)
-			
-			Booru.search('safebooru', 'glaceon', { limit: 1, random: true })
-  .then(posts => {
-    if (posts.length === 0) {
-      console.log('No images found.')
-    }
-
-    for (let post of posts) {
-      message.send(post.fileUrl)
-    }
-  })
-  .catch(err => {
-    if (err instanceof BooruError) {
-      // It's a custom error thrown by the package
-      // Typically results from errors the boorus returns, eg. "too many tags"
-      console.error(err)
-    } else {
-      // This means something pretty bad happened
-      console.error(err)
-    }
-  })
 		).catch((error) => console.log);
 		
 	}
