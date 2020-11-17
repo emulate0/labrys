6fdba3beee973944e906f7b6616064ca1eec25c7
typack <39818656+emulate0@users.noreply.github.com>
2020-11-16 21:01:26 -0600

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -6,10 +6,10 @@ module.exports = {
 	description: 'Generates a random number within the limit specified.',
 	execute: (message, args) => {
 		var totalNum = 0
-		var i/*
-		for (i = 0; i < args[0]; i++)
+		var i
+		for (i = 0; i < args[0]; i++){
 			totalNum += (Math.floor(Math.random() * args[1]) + 1
-			*/
+		}
 		return message.channel.send(new MessageEmbed()
 			.setColor(message.client.colors.INFO)
 			.setTitle('Rolled ' + args[0] + ' D' + args[1])
