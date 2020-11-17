e1e7fb2c072e8f0624225e06a3cb66aa95542cf9
typack <39818656+emulate0@users.noreply.github.com>
2020-11-16 21:11:00 -0600

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	names: ['roll'],
-	usage: 'roll [#]',
+	usage: 'roll [# of dice] [sides on die]',
 	description: 'Generates a random number within the limit specified.',
 	execute: (message, args) => {
 		var totalNum = 0
@@ -11,8 +11,8 @@ module.exports = {
 			totalNum += Math.floor(Math.random() * args[1]) + 1
 		}
 		return message.channel.send(new MessageEmbed()
-			.setColor(message.client.colors.INFO)
-			.setTitle('Rolled ' + args[0] + ' D' + args[1])
+			.setColor(message.client.colors.SUCCESS)
+			.setTitle('Rolled [' + args[0] + '] D' + args[1])
 			.addField('Got ', totalNum)
 		).catch((error) => console.log);
 	}
